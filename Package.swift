ac772e3e1f4fea9a620189a6ee9833e6885e341d
Tabber-shoplive <tabber@shoplive.cloud>
2025-07-06 14:56:58 +0900

    name changed

--- Package.swift
+++ Package.swift
@@ -8,14 +8,16 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SRT-Test",
-            targets: ["SRT-Test"]),
+            name: "libsrt",
+            targets: ["libsrt"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SRT-Test"),
+            name: "libsrt",
+            path: "./Sources/SRT-Test/libsrt.xcframework"
+        ),
 
     ]
 )
